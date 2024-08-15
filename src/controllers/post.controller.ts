8a9634f3cb98ea11af933a45175d6de154bf0bb1
Ritul Bhatnagar <ritulbhatnagar15@gmail.com>
2024-08-16 00:38:20 +0530

    added the missing code:Post contoller

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -2,3 +2,28 @@ import { Request, Response } from "express";
 import logger from "../utils/logger";
 import APIError, { HttpStatusCode } from "../middlewares/errorMiddleware";
 import { localConstant } from "../utils/constant";
+import { createPostService } from "../services/post.service";
+
+export const createPost = async (req: Request, res: Response) => {
+  const { content, title } = req.body;
+  const authorId = req.body.user.id;
+  try {
+    const post = await createPostService(authorId, content, title);
+    return res.status(HttpStatusCode.CREATED).json({
+      message: "Post created successfully",
+      post,
+    });
+  } catch (error) {
+    logger.error("Error while creating post", error);
+    if (error instanceof APIError) {
+      return res
+        .status(error.httpCode || HttpStatusCode.INTERNAL_SERVER_ERROR)
+        .json({
+          message: error.message || localConstant.POST_NOT_CREATED,
+        });
+    }
+    return res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json({
+      message: localConstant.POST_NOT_CREATED,
+    });
+  }
+};
